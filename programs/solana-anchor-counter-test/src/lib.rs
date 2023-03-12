20b04b378ce73c36b544c6b2d5cc0899c414b39c
Antonio Molinari <molinari@incode.it>
2023-03-12 13:22:29 +0100

    wip

--- programs/solana-anchor-counter-test/src/lib.rs
+++ programs/solana-anchor-counter-test/src/lib.rs
@@ -7,10 +7,44 @@ pub mod solana_anchor_counter_test {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
 
+// contexts for the functions
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = Counter::SIZE)]
+    // (* vedi sotto) quando si inizializza un nuovo account bisogna specificare la dimenisone,
+    // per questo viene preso il valore dalla proprietà che abbiamo creato sotto
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+}
+
+
+// data structure for the state
+#[account]
+pub struct Counter {
+    pub count: u64, // 8 bytes
+}
+
+impl Counter {
+    pub const SIZE: usize = 8 + 8; // 8 bytes + 8 bytes for the counter
+}
--- tests/solana-anchor-counter-test.ts
+++ tests/solana-anchor-counter-test.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { expect } from "chai";
 import { SolanaAnchorCounterTest } from "../target/types/solana_anchor_counter_test";
 
 describe("solana-anchor-counter-test", () => {
@@ -7,10 +8,41 @@ describe("solana-anchor-counter-test", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SolanaAnchorCounterTest as Program<SolanaAnchorCounterTest>;
+  const counterAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accounts({
+      counter: counterAccount.publicKey,
+      user: provider.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).signers({
+      counterAccount
+    }).rpc();
+    const counter = await program.account.counter.fetch (counterAccount.publicKey);
   });
+
+  it ("Is possible to increment the counter", async() => {
+    const counterBefore = await program.account.counter.fetch(
+      counterAccount.publicKey
+    );
+    console.log("Counter Before", counterBefore.count.toNumber());
+
+    const tx = await program.methods
+    .increment()
+    .accounts({
+      counter: counterAccount.publicKey,
+    })
+    .rpc({ commitment: "confirmed" });
+    console.log("Your transaction signature", tx);
+    
+    const counterAfter = await program.account.counter.fetch(
+      counterAccount.publicKey
+    );
+    console.log("Counter After", counterAfter.count.toNumber());
+
+    expect(counterAfter.count.toNumber()).to.be.greaterThan(counterBefore.count.toNumber());
+  });
+
 });
+
+
